d62ecb3b28757757cb44d381d4d12997c2f2c783
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-04-25 20:19:22 -0400

    Testing practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import "./App.css";
 // App.jsx
 
-const App = () => <h1>Our First Test</h1>;
+// App.jsx
+
+import { useState } from "react";
+
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
 
 export default App;
--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,11 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
+// App.test.jsx
 
+import userEvent from "@testing-library/user-event";
 import App from "../src/App";
-
 describe("App component", () => {
-  it("renders correct heading", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
     render(<App />);
-    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
